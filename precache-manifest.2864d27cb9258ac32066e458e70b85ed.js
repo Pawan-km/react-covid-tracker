e88584b2e3fa4280716ae824dc2f4a0c2549ce3f
Pawan <pranaykunar.1461@gmail.com>
2021-12-30 09:01:14 +0530

    Updates

---
+++ precache-manifest.2864d27cb9258ac32066e458e70b85ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66ae95d88bd974395fd18656c311ea2",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "b8e12eba438698910683",
+    "url": "/react-covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b8fb4332983dd8f4a5e3",
+    "url": "/react-covid-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "b8e12eba438698910683",
+    "url": "/react-covid-tracker/static/js/2.855e3be4.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/react-covid-tracker/static/js/2.855e3be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8fb4332983dd8f4a5e3",
+    "url": "/react-covid-tracker/static/js/main.1961da34.chunk.js"
+  },
+  {
+    "revision": "261fd4b71819be31f984",
+    "url": "/react-covid-tracker/static/js/runtime-main.803e8507.js"
+  }
+]);
